4a2b31d79b06dcd43a14e4ab578f1863456790a7
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-08-24 13:57:59 +0200

    Stunde vom 24.08.2022

--- 01 Einstieg/Auto.java
+++ 01 Einstieg/Auto.java
@@ -2,4 +2,33 @@ public class Auto {
     // Variablen
     private String hersteller; // z.B. VW, Volvo, BMW
     private String modell;     // z.B. Golf, M3
+    private String kennzeichen;
+    private String farbe;
+    private String motor;
+
+    // Funktionen
+    public void hupe() {
+        System.out.println(kennzeichen + ": Huuuuuuuuuuup!");
+    }
+
+    // Setter-Methoden
+    // Helfen, Daten in ein Objekt hineinzubekommen
+    // f(x) = 2x
+    // f(3) = 2*3 = 6
+    public void setHersteller(String neuerHersteller) {
+        // Wertezuweisung 
+        // <VAR> = <Wert>
+        // Setze den Wert von <hersteller> auf
+        // den Wert von Parameter <neuerHersteller>
+        hersteller = neuerHersteller;
+    }
+
+    public void setModell(String neuesModell) {
+        modell = neuesModell;
+    }
+
+    public void setKennzeichen(String neuesKennzeichen) {
+        kennzeichen = neuesKennzeichen;
+
+    }
 }
---
+++ 01 Einstieg/AutoHaendler.java
@@ -0,0 +1,14 @@
+public class AutoHaendler {
+    private Auto auto1;
+    private Auto auto2;
+    private Auto auto3;
+    
+    public void main() {
+        System.out.println("Willkommen bei Kühnert's Karren");
+        auto1 = new Auto();
+        auto1.setHersteller("VW");
+        auto1.setModell("Golf");
+        auto1.setKennzeichen("LEV MS-0");
+        auto1.hupe();
+    }
+}
