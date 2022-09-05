1dbca33ab596b66ac91867c47683cb0bf445b8ac
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-09-05 13:45:28 +0200

    Stunde vom 05.09.2022

--- 01 Einstieg/Seifenspender.java
+++ 01 Einstieg/Seifenspender.java
@@ -1,11 +1,40 @@
 public class Seifenspender {
     // Variablen
     private int inhalt;
-    
+    private int kapazitaet;
+    private int ausgabemenge;
+    private String seifenart;
     
     // Funktionen
+    public Seifenspender() {
+        // Konstruktor
+        // 1. Kein void
+        // 2. Heißt genau so wie die Klasse
+        // 3. Wird automatisch beim Erzeugen eines Objekts
+        //    ausgeführt
+        kapazitaet = 200;
+        ausgabemenge = 67;
+        seifenart = "Lavendel";
+        inhalt = 4 * ausgabemenge;
+    }
+    
     public void spenden() {
+        // Subtrahiere <ausgabemenge> von <inhalt>
+        // Und speichere das Ergebnis in <inhalt>
+        inhalt = inhalt - ausgabemenge;
+        System.out.println("Hier sind " + ausgabemenge + "ml Seife.");
         
     }
     
+    public void nachfuellen() {
+        
+    }
+    
+    public void druckeStatus() {
+        
+    }
+    
+    public void setInhalt(int neuerInhalt) {
+        inhalt = neuerInhalt;
+    }
 }
