d5c8129204d7cff480f4586d65b98e3747820e20
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-09-07 12:30:24 +0200

    Stunde vom 07.09.2022

--- 01 Einstieg/Seifenspender.java
+++ 01 Einstieg/Seifenspender.java
@@ -4,7 +4,9 @@ public class Seifenspender {
     private int kapazitaet;
     private int ausgabemenge;
     private String seifenart;
-    
+    // Wahrheitswert; ist entweder true (wahr) oder false (unwahr)
+    private boolean eingeschaltet;
+
     // Funktionen
     public Seifenspender() {
         // Konstruktor
@@ -12,29 +14,55 @@ public class Seifenspender {
         // 2. Heißt genau so wie die Klasse
         // 3. Wird automatisch beim Erzeugen eines Objekts
         //    ausgeführt
+        System.out.println("Ein neuer Seifenspender ist geboren");
         kapazitaet = 200;
         ausgabemenge = 67;
         seifenart = "Lavendel";
-        inhalt = 4 * ausgabemenge;
+        setInhalt(5 * ausgabemenge);
+        eingeschaltet = false;
     }
-    
+
     public void spenden() {
-        // Subtrahiere <ausgabemenge> von <inhalt>
-        // Und speichere das Ergebnis in <inhalt>
-        inhalt = inhalt - ausgabemenge;
-        System.out.println("Hier sind " + ausgabemenge + "ml Seife.");
-        
+        // Spende nur Seife, wenn das Gerät eingeschaltet ist
+        if (eingeschaltet == true) {
+            // Subtrahiere <ausgabemenge> von <inhalt>
+            // Und speichere das Ergebnis in <inhalt>
+            inhalt = inhalt - ausgabemenge;
+            System.out.println("Hier sind " + ausgabemenge + "ml Seife.");
+        } else {
+            System.out.println("FEHLER: Gerät ist ausgeschaltet");
+        }
     }
-    
-    public void nachfuellen() {
-        
-    }
-    
-    public void druckeStatus() {
-        
-    }
-    
+
     public void setInhalt(int neuerInhalt) {
-        inhalt = neuerInhalt;
+        // Wenn neuerInhalt hineinpasst
+        // D.h. neuerInhalt ist kleiner-gleich Kapazität
+        if (neuerInhalt <= kapazitaet) {
+            // Setze inhalt auf neuerInhalt
+            inhalt = neuerInhalt;
+        } else {
+            // Ansonsten: Setze inhalt auf das Maximum
+            inhalt = kapazitaet;
+        }
+
+        System.out.println("Inhalt ist jetzt: " + inhalt + " ml");
+    }
+
+    public void nachfuellen() {
+        setInhalt(kapazitaet);
+    }
+
+    public void druckeStatus() {
+
+    }
+
+    public void einschalten() {
+        eingeschaltet = true;
+        System.out.println("Der Spender ist jetzt ON.");
+    }
+
+    public void ausschalten() {
+        eingeschaltet = false;
+        System.out.println("Der Spender ist jetzt OFF.");
     }
 }
