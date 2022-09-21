bc9088f3d7bee9e7828c8dfefecc6cc83e195794
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-09-21 13:58:07 +0200

    Stunde vom 21.09.2022

--- 01 Einstieg/Ticketautomat.java
+++ 01 Einstieg/Ticketautomat.java
@@ -4,34 +4,57 @@ public class Ticketautomat {
     private Scanner tastatur;
     private int ticketAnzahl;
     private int ticketPreis;
-    private int guthaben;
+    private int restbetrag;
 
     public Ticketautomat() {
         // Initialisierung
         tastatur = new Scanner(System.in);
         ticketAnzahl = -1;
         ticketPreis = 10;
-        guthaben = 0;
+        restbetrag = 0;
     }
-    
+
     public void druckeTicket() {}
+
     public void druckeTickets() {}
-    
+
     public void waehleAnzahl() {
         System.out.print("Wie viele Tickets? ");
+        // Lies eine Zeile Text über Tastatur ein
+        // und speichere Ergebnis in <eingabe>
         String eingabe = tastatur.nextLine();
         System.out.println("Deine Eingabe: '"+eingabe+"'");
+
+        // Wandele <eingabe> in Zahl um
+        ticketAnzahl = Integer.parseInt(eingabe);
+        System.out.println("Du willst also " + ticketAnzahl + " Tickets.");
+        restbetrag = ticketPreis * ticketAnzahl;
     }
-    
+
     public void gibRueckgeld() {}
-    public void nimmGeldAn() {}
+
+    public void nimmGeldAn() {
+        // Rechne/Gib den Restbetrag aus, den der Benutzer 
+        // zahlen muss
+        System.out.println("Fehlend: " + restbetrag + " €.");
+
+        // Lies mit der Tastatur ein, welches Geldstueck 
+        // der Benutzer einfuettert
+        System.out.print("Wirf ein Geldmittel ein: ");
+        // Verrechne das mit dem offenen Betrag
+        String eingabe = tastatur.nextLine();
+        int geldstueck = Integer.parseInt(eingabe);
+        restbetrag = restbetrag - geldstueck;
+
+        System.out.println("Fehlend: " + restbetrag + " €.");
+    }
 
     public void begruesse() {
         System.out.println("❤---------------------------👍");
         System.out.println("Willkomen bei Arnold Trains");
         System.out.println("Eine Fahrkarte kostet " + ticketPreis + " €.");
     }
-    
+
     public void druckeMenu() {
         begruesse();
         waehleAnzahl();
