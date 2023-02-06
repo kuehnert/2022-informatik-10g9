5e8174f2114e6c2837343bab5b3bf1409e7b7167
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2023-02-06 13:06:58 +0100

    Stunde vom 06.02.2023

--- 02 Arrays G9/Arrays1.java
+++ 02 Arrays G9/Arrays1.java
@@ -7,17 +7,17 @@ public class Arrays1 {
         // Deklaration
         int zahl;
         int[] zahlen;
-        
+
         // Initialisierung
         zahl = 5;
         zahlen = new int[]{1, 2, 3, 4, 5};
-        
+
         // Ausgabe / Lesender Zugriff
         // System.out.println(zahl);
 
         // Nicht hilfreich: Nur Speicheradresse
         System.out.println(zahlen);
-        
+
         // Elemente einzeln über ihren Index ausgeben
         // System.out.println( zahlen[0] ); // => 1
         // System.out.println( zahlen[2] ); // => 3
@@ -34,9 +34,13 @@ public class Arrays1 {
         System.out.println("Taras Zahlen: ");
         int[] zweitesArray = new int[]{10, 15, 3};
         gibArrayAus(zweitesArray);
+        gibArrayVerdoppeltAus(zweitesArray);
+        gibArrayAus(zweitesArray);
     }
-    
 
+    // Mathe-Funktion: f(x) = x^2 + 1
+    // g(x)
+    // f'(x)
     public void gibArrayAus(int[] a) {
         // Gib mithilfe eines Zählers und einer
         // while-Schleife nacheinander jedes Element
@@ -48,14 +52,27 @@ public class Arrays1 {
             zaehler = zaehler + 1;
         }
     }
-    
+
     /*
      * Hausaufgabe:
      * Schreibe eine Methode, die alle Werte in einem Array
      * verdoppelt ausgibt
      */
-    
-    
-    
+    public void gibArrayVerdoppeltAus(int[] a) {
+        int zaehler = 0;
+
+        while (zaehler < a.length) {
+            System.out.println( a[zaehler] * Math.PI );
+            zaehler = zaehler + 1;
+        }
+    }
+
+    /*
+     *  1. Ändere gibArrayAus so um, dass alle Zahlen
+     *     durch Komma getrennt in einer Zeile ausgegeben werden
+     *  2. Ändere gibArrayVerdoppeltAus so um in "verdoppele",
+     *     dass es die Werte dauerhaft im Array verdoppelt
+     *     (aber nicht mehr auf dem Bildschirm ausgibt)
+     */
     
 }
