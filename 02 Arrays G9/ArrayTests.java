eda9ee263c1e1b1a452f1a7a650c802857b59fe9
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2023-03-22 12:07:12 +0100

    Stunde vom 22.03.2023

--- 02 Arrays G9/ArrayTests.java
+++ 02 Arrays G9/ArrayTests.java
@@ -1,11 +1,43 @@
 public class ArrayTests {
-    public void testeArrayErzeugen() {
-        // Hausaufgabe 20.03.23:
-        // Erstelle einen gen vom Typ ArrayGeneratoren
-        // und rufe gen.arrayErzeugen1() auf
-        // ... und verwende gibArrayAus aus ArrayWerkzeuge 
-        // um es auf dem Bildschirm auszugeben
-        arrayErzeugen1();
+    private ArrayGeneratoren generator; // Deklaration
+    private ArrayWerkzeuge werkzeuge;
+
+    // Konstruktor
+    public ArrayTests() {
+        System.out.println("Konstruktor wird aufgerufen");
+        generator = new ArrayGeneratoren(); // Initialisierung
+        werkzeuge = new ArrayWerkzeuge();
     }
 
+    public void testeArrayErzeugen() {
+        System.out.println("testeArrayErzeugen()");
+        // Hausaufgabe 20.03.23:
+        // 1. Erstelle einen generator vom Typ ArrayGeneratoren
+
+        // 2. Rufe gen.arrayErzeugen1() auf
+        int[] fritz  = generator.arrayErzeugen1(7, 8);
+        int[] hans   = generator.arrayErzeugen1(5, 6);
+        int[] willi  = generator.arrayErzeugen1(9, 7);
+
+        // 3. Verwende gibArrayAus aus ArrayWerkzeuge 
+        // um es auf dem Bildschirm auszugeben
+        werkzeuge.gibArrayAus( fritz );
+        werkzeuge.gibArrayAus( hans  );
+        werkzeuge.gibArrayAus( willi );
+    }
+
+    public void testeArrayErzeugen2() {
+        System.out.println("testeArrayErzeugen2()");
+        // Erzeuge drei Arrays mit 
+        // arrayErzeugen2, arrayErzeugen3 & arrayErzeugen3c
+        int[] a1 = generator.arrayErzeugen2(10);
+        int[] a2 = generator.arrayErzeugen3(10);
+        int[] a3 = generator.arrayErzeugen3c(10);
+
+        // speichere sie als a1, a2, a3
+        // und gib sie als Tabelle
+        werkzeuge.gibArrayAusTab(a1);
+        werkzeuge.gibArrayAusTab(a2);
+        werkzeuge.gibArrayAusTab(a3);        
+    }
 }
---
+++ 02 Arrays G9/Arrays2.java
@@ -0,0 +1,13 @@
+public class Arrays2 {
+    // Aufgaben
+    // Schreibe Methoden, die dieses tun:
+    // 1. Ein Array rückwärts ausgeben
+    //       {6, 7, 3} => "3 7 6 "
+    // 2. Alle Zahlen in einem Array aufsummieren
+    // 3. Aus allen Zahlen in einem Array den Mittelwert bestimmen
+    // 4. Alle positiven Elemente eines Arrays summiert 
+    //       {2, -5, 8} => 10
+    // 5. Zwei Arrays aneinanderhängen und als neues Array z'geben:
+    //       {6, 7, 3}, {2, -5, 8} => {6, 7, 3, 2, -5, 8}
+    // 6. Qualität des Zufallszahlengenerators... Teaser!
+}
