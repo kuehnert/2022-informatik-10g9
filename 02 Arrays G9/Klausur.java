05c05ce9b27a30c4bdb5b108c23193a064b918fd
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2023-03-27 13:22:57 +0200

    Stunde vom 27.03.2023

--- 02 Arrays G9/Klausur.java
+++ 02 Arrays G9/Klausur.java
@@ -1,8 +1,34 @@
+import java.util.Random;
+
 public class Klausur {
     public void a1() {
-        
-        for (int y = 1; y <= 18; y = y + 3) {
-            System.out.print(y + " ");
+        {
+            int z = 0;
+            while (z < 10) {
+                System.out.print(z + " ");
+                z = z + 2;
+            }
         }
+
+        for (int z = 0; z < 10; z = z + 2) {
+            System.out.print(z + " ");
+        }
+
+        int i = 10;
+        while (i > 0) {
+            System.out.print("# ");
+            i = i - 1;
+        }
+    }
+
+    public int[] zufallszahlen(int anzahl) {
+        Random generator = new Random();
+        int[] a = new int[10];
+
+        for (int i = 0; i < a.length; i = i + 1) {
+            a[i] = generator.nextInt(5);
+        }
+        
+        return a;
     }
 }
