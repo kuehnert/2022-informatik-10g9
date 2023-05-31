97f810f1da378df5c5fa9296688c5c160ebb2c24
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2023-05-31 11:23:20 +0200

    Zwischenstand

--- 02 Arrays G9/Zahlwoerter.java
+++ 02 Arrays G9/Zahlwoerter.java
@@ -7,24 +7,42 @@
  *  2784   => 
  */
 public class Zahlwoerter {
+    String[] THOUSANDS = {"", " thousand", " million", " billion"};
     String[] DECADE_WORDS = { null, null, "twenty", "thirty", "forty", "fifty",
             "sixty", "seventy", "eighty", "ninety"};
-    String[] NUMBER_WORDS = {"zero", "one", "two", "three",
+    String[] NUMBER_WORDS = {"", "one", "two", "three",
             "four", "five", "six", "seven", "eight", "nine", "ten",
             "eleven", "twelve", "thirteen", "fourteen", "fifteen", 
             "sixteen", "seventeen", "eighteen", "nineteen", "twenty"};
 
+    // 74.074.074.074
     public String zahlAufEnglisch(int zahl) {
-        if (zahl <= 20) {
-            return NUMBER_WORDS[zahl];
-        } else if (zahl < 100) {
-            int letzteZiffer = zahl % 10;
-            int vorletzteZiffer = zahl / 10 % 10;
-            String letzteZifferStr = NUMBER_WORDS[letzteZiffer];
-    
-            return DECADE_WORDS[vorletzteZiffer] + "-" + letzteZifferStr; 
+        if (zahl == 0) {
+            return "nought";
         }
-        
-        return "BLUB";
+
+        String ausgabe = "";
+        // Hunderter
+        int hundred = zahl / 100 % 10;
+        int vorletzteZiffer = zahl / 10 % 10;
+        int letzteZiffer = zahl % 10;
+        // 1711 => 7
+        // 8 => 0
+        // 1077 => 0
+        // 453624523647 => 6
+        if (hundred > 0) {
+            // ausgabe = ausgabe + ...
+            ausgabe += NUMBER_WORDS[hundred] + " hundred ";
+        }
+
+        if (vorletzteZiffer < 2) {
+            ausgabe += NUMBER_WORDS[zahl % 100];
+        } else {
+            String letzteZifferStr = NUMBER_WORDS[letzteZiffer];
+
+            ausgabe += DECADE_WORDS[vorletzteZiffer] + "-" + letzteZifferStr; 
+        }
+
+        return ausgabe;
     }
 }
